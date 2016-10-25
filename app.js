c16eb97ee6ee3f3c46b6e3b33fe0ee90bbe8736d
jmhans <jmhans@hotmail.com>
2016-10-25 08:58:08 -0500

    added files

---
+++ app.js
@@ -0,0 +1,8 @@
+﻿
+$("msg").html("say something");
+
+$.get("https://spreadsheets.google.com/feeds/list/0AtMEoZDi5-pedElCS1lrVnp0Yk1vbFdPaUlOc3F3a2c/od6/public/values?alt=json-in-script&callback=x", function (data) {
+    $("msg").html(data);
+    alert("Load was performed.");
+});
+
