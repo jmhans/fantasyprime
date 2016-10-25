41521b5e8d83c5ede9fe262b6cf742725a7f1bf0
jmhans <jmhans@hotmail.com>
2016-10-25 11:00:33 -0500

    updating app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-﻿$("msg").html("say something");
+﻿$("#msg").html("say something");
 
 //Added a comment
 
 $.get("https://spreadsheets.google.com/feeds/list/0AtMEoZDi5-pedElCS1lrVnp0Yk1vbFdPaUlOc3F3a2c/od6/public/values?alt=json-in-script&callback=x", function (data) {
-    $("msg").html(data);
+    $("#msg").html(data);
     alert("Load was performed.");
 });
