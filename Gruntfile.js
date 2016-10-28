1bd25f3ae8f3ff176146e4fcfa81c37af42be274
jmhans <jmhans@hotmail.com>
2016-10-28 11:27:17 -0500

    Added Gruntfile and minification

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['app/**/*.js'],
+                dest: 'app/dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                files: {
+                    'app/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'app/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
