e9f8c99b5564d4c5ee9f4d92b52d27c5a59d74fa
Justin Hanson <jmhans@hotmail.com>
2016-10-29 21:27:52 -0500

    Re-ordering js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,9 @@
                 separator: ';'
             },
             dist: {
-                src: [
+                src: [  'app/bower_components/angular/angular.js',
                         'app/bower_components/angular-route/angular-route.js',
-                        'app/bower_components/angular/angular.js',
-                        'app/js/*.js'
-                ],
+                        'app/js/*.js'],
                 dest: 'app/dist/<%= pkg.name %>.js'
             }
         },
