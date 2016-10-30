03c95811da0bba7d4b200737bad03e8382e199a3
Justin Hanson <jmhans@hotmail.com>
2016-10-29 20:47:33 -0500

    Updated onLoad function

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,7 @@ angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
       $scope.$on('$viewContentLoaded', function () {
 
-          document.getElementById("msg").innerHTML = "Paragraph changed!"
-
-          //Added a comment
+            //Added a comment
 
           var url = "https://spreadsheets.google.com/feeds/list/1-T37CNjD3u4mO2p21rKbajCPgyhigC-M9pySexxF_Pg/om6s0f5/public/basic?alt=json";
           $.ajax({
@@ -16,6 +14,7 @@ angular.module('myApp.controllers', [])
               dataType: "jsonp",
               success: function (data) {
                   // data.feed.entry is an array of objects that represent each cell
+                  document.getElementById("msg").innerHTML = "Spreadsheet Data Retrieved"
               },
           })
 
