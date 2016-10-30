614e67eb63ece20c40b9e8209c623a307ccfb753
Justin Hanson <jmhans@hotmail.com>
2016-10-29 20:55:52 -0500

    Incorporated $http method in controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,20 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
       $scope.$on('$viewContentLoaded', function () {
 
             //Added a comment
 
           var url = "https://spreadsheets.google.com/feeds/list/1-T37CNjD3u4mO2p21rKbajCPgyhigC-M9pySexxF_Pg/om6s0f5/public/basic?alt=json";
-          $.ajax({
-              url: url,
-              dataType: "jsonp",
-              success: function (data) {
-                  // data.feed.entry is an array of objects that represent each cell
-                  document.getElementById("msg").innerHTML = "Spreadsheet Data Retrieved"
-              },
-          })
+          $http.get(url).then(function (data) {
+              document.getElementById("msg").innerHTML = "Spreadsheet Data Retrieved";
+          }
+          );
 
       });
   }])
