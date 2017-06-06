4b2c8b69888c89d6726126e4fcfe4b956abfb190
jmhans <jmhans@hotmail.com>
2017-06-06 12:58:54 -0500

    Included selectable year on prop bet page.

--- services/propbetService.js
+++ services/propbetService.js
@@ -2,33 +2,8 @@
 angular.module('fantasyfantasy').service('propBetService', function ($http) {
     var service = {
         
-        getBBData: function () {
-            var prom = new Promise(function (resolve, reject) {
-                
-                return Papa.parse('data/baseballdata.csv?q="+Math.random()', {
-                    download: true,
-                    delimiter: ",",
-                    dynamicTyping: true,
-                    header: false,
-                    skipEmptyLines: true,
-                    complete: function (results, file) {
-                        resolve( results.data);
-                    }
-                });
-
-            });
-            return prom;
-
-        },
         getBBDataJSON: function () {
             return $http.get("data/baseballdata.json").then(function (resp) {
-                /*var keys = resp.data.shift();
-                resp.data = resp.data.map(function (row) {
-                    return keys.reduce(function (obj, key, i) {
-                        obj[key] = row[i];
-                        return obj;
-                    }, {});
-                });*/
                 return resp.data
             });
 
@@ -43,24 +18,19 @@ angular.module('fantasyfantasy').service('propBetService', function ($http) {
                     }, {});
                 });
 
+                resp.data.forEach(function (obj) {
+                    obj.SafeTitle = obj.Title.replace(/\s/g, '_');
+                });
+
                 return resp.data;
             });
 
         },
-        getConfigData: function () {
-            var prom = new Promise(function (resolve, reject) {
-                return Papa.parse('data/Configuration.csv?q="+Math.random()', {
-                    download: true,
-                    delimiter: ",",
-                    dynamicTyping: true,
-                    header: true,
-                    skipEmptyLines: true,
-                    complete: function (results, file) {
-                        resolve(results.data);
-                    }
-                });
+        getBetSummaryJSON: function () {
+            return $http.get("data/bet.json").then(function (resp) {
+                return resp.data;
             });
-            return prom;
+
         }
     };
 
